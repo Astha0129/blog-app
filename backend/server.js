604d1e72560043ef8ee2bf4cd1e6a234f5045c51
Aastha Dubey <asthad431@gmail.com>
2026-07-11 02:07:00 +0530

    Update CORS configuration

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   process.env.FRONTEND_URL,
   "http://localhost:5173",
+  "https://blog-app-gilt-ten-24.vercel.app",
 ].filter(Boolean);
 
 app.use(
